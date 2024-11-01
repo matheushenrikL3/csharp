adc2a0ab298f8b156fe2f1f0d480261b776d028e
matheushenrikL3 <marcelineschiavon@gmail.com>
2024-11-01 17:17:54 -0400

    Operadores Aritimetico

--- operadores/Program.cs
+++ operadores/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+double resultado1 = 10 + 8 + 12 / 3;
+Console.WriteLine(resultado1);
+
+double resultado2 = (10 + 8 + 12) / 3;
+Console.WriteLine(resultado2);
+
+double resultado3 = (2 + 8) * 2 / 4;
+Console.WriteLine(resultado4);
