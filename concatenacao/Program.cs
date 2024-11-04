f7e66bb0513c01f6ef535c32a465dbcbd885be7e
matheushenrikL3 <marcelineschiavon@gmail.com>
2024-11-04 17:00:23 -0400

    atualizacao

--- concatenacao/Program.cs
+++ concatenacao/Program.cs
@@ -1,6 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
 // Concatenção
 string nome = "erick";
 string saudacao = "Ola," + nome + "! Bem-vindo.";
+Console.WriteLine(saudacao);
+
+// Iterpolcao de strings($)
+string nome = "erick";
+string saudacao = $"Ola, {nome}! Bem-vindo.";
 Console.WriteLine(saudacao);
