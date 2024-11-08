5259b6530d3b783f2c63d335dd316a18ef79c68c
matheushenrikL3 <marcelineschiavon@gmail.com>
2024-11-08 16:32:16 -0400

    adicionar  exercicio

--- exercicio/Program.cs
+++ exercicio/Program.cs
@@ -1,5 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-int idade = 18
-string mensagem = idade >= 18 ? "Entrada liberada" : "Menor de idade";
-Console.WriteLine(mensagem);
+﻿/*int idade = 18;
+string mensagem = idade >= 18 ? "Maior de idade";
+Console.WriteLine("Entrada Liberada");
+
+int idade = 18;
+string mensagem = idade <= 18 ? "Menor de idade";
+Console.WriteLine("Entrada nao permitida");
+*/
+/*int idade1 >= 18;
+string entrada1 = $"Qual é a sua idade? {idade1}. Entrada permitida";
+Console.WriteLine(entrada1);
+
+int idade2 <= 17;
+string entrada2 = $"Qual é a sua idade? {idade2}. Entrada nao permitida";
+Console.WriteLine(entrada2);
+*/
+// Exercícios
+
+bool ehMaiorIdade = false;
+bool possuiAutorizacaoResponsavel = true;
+
+if(ehMaiorIdade || possuiAutorizacaoResponsavel);
+{
+    Console.WriteLine("Entreda liberada");
+}
+else
+{
+    Console.WriteLine("Entreda liberada");
+}
+
+bool ehMaiorIdade = false;
+bool possuiAutorizacaoResponsavel = true;
+
+string resultado = ehMaiorIdade || possuiAutorizacaoResponsavel ? "Entrada liebrada" : "Entrada nao liberada";
+
+Console.WriteLine(resultado);
